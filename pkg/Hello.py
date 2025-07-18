e511710f6fce207401a6f728a67cbf66d1fc6207
ajtw7 <andrew.worsley91@gmail.com>
2025-07-18 15:43:14 -0400

    Resoled

--- pkg/Hello.py
+++ pkg/Hello.py
@@ -1,11 +1,5 @@
 class Hello:
     @staticmethod
     def hello_world() -> str:
-        """ hello_world
-        This method should return the string "Hello, World!".
-        Args:
-            None
-        Returns:
-            str: "Hello, World!"
-        """
-        return None
+        
+        return "Hello, World!"
